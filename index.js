5b0503f91244de1ca477dcca020ab250f431b0c1
Christian Tavares <chrisadrianotav@gmail.com>
2017-12-02 05:12:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 var recipes = new Object
-recipes = {[key]:value}
+recipes = {[snack]:eminems}
 
-function updateObjectWithKeyAndValue(object,key,value){
-  var obj = recipes
-  Object.assign({}, obj, {['key']:value});
-  return recipes
+function updateObjectWithKeyAndValue(obj,key,value){
+  return Object.assign({}, obj, {[key]:value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj,key,value){
+  obj[key] = value;
+  return obj
+}
+function destructivelyDeleteFromObjectByKey(obj,key){
+  delete obj[key];
+  return obj;
+}
+function deleteFromObjectByKey(obj,key,value){
+  var obj = {key:value};
+  var newObj = Object.assign({},obj);
+  delete newObj.key;
+  return obj;
 }
